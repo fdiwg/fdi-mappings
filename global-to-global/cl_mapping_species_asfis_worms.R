3ae12058b2d9d450d9cf998d2864a1c2c5342d4a
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2025-09-11 13:21:13 +0200

    Update cl_mapping_species_asfis_worms.R #13

--- global-to-global/cl_mapping_species_asfis_worms.R
+++ global-to-global/cl_mapping_species_asfis_worms.R
@@ -5,4 +5,11 @@ asfis = readr::read_csv("https://raw.githubusercontent.com/fdiwg/fdi-codelists/r
 asfis$AphiaID = unlist(get_WoRMS_AphiaIDs(asfis$definition, parallel = FALSE)) #parallelization returns very few matching results
 asfis$isscaap_div_code = sapply(asfis$isscaap_group_code, substr,1,1)
 asfis$has_AphiaID = !is.na(asfis$AphiaID)
-table(asfis[,c("isscaap_div_code", "has_AphiaID")])
+
+asfis_worms_mapping <- data.frame(
+    src_code = asfis$code,
+    trg_code = asfis$AphiaID,
+    src_codingsystem = "asfis",
+    trg_codingsystem = "worms"
+)
+readr::write_csv(asfis_worms_mapping, "./global-to-global/cl_mapping_species_asfis_worms.csv")
